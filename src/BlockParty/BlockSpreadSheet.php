2a97cbea4549a18634e5d6e871eaf9af4c9d5f00
Justin L Sanders <justin@pointw.com>
2019-12-17 23:07:35 -0600

    Add Spreadsheet class

---
+++ src/BlockParty/BlockSpreadSheet.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace BlockParty;
+
+use PhpOffice\Spreadsheet\Spreadsheet;
+
+class BlockSpreadSheet extends Spreadsheet {
+
+    public function __construct() {
+        parent::__construct();
+        $this->disconnectWorksheets();
+        $this->createWorksheet();
+    }
+
+    /**
+     * Create sheet and add it to this workbook.
+     *
+     * @param null|int $sheetIndex Index where sheet should go (0,1,..., or null for last)
+     *
+     * @throws Exception
+     *
+     * @return Worksheet
+     */
+    public function createSheet($sheetIndex = null)
+    {
+        $newSheet = new BlockWorksheet($this);
+        $this->addSheet($newSheet, $sheetIndex);
+
+        return $newSheet;
+    }
+}
