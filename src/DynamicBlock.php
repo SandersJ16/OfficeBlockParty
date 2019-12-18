82d0e92d04f041db0b2f01b13ba2e489cfd56ce1
Justin L Sanders <justin@pointw.com>
2019-12-18 02:13:05 -0600

    Added tests and code for adding cells and height

--- src/DynamicBlock.php
+++ src/DynamicBlock.php
@@ -2,8 +2,20 @@
 
 namespace BlockParty;
 
+use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
+
 class DynamicBlock implements Block
 {
+    /**
+     * The cells that will be rendered by this block,
+     * it's a multidimensional array with the row index
+     * as the first key and the column index as the second
+     * key (both column and key indexes start at 1)
+     *
+     * @var array
+     */
+    private $cells = array();
+
     /**
      * Get the height of this XlsxBlock
      *
@@ -11,7 +23,7 @@ class DynamicBlock implements Block
      */
     public function getHeight()
     {
-        return 0;
+        return $this->cells ? max(array_keys($this->cells)) : 0;
     }
 
     /**
@@ -22,5 +34,22 @@ class DynamicBlock implements Block
     public function getWidth()
     {
         return 0;
+    }
+
+    /**
+     * Add a cell to this block
+     *
+     * @param  string $cell The cell's name
+     * @param  mixed $data
+     *
+     * @return self
+     */
+    public function addCell($cell, $data)
+    {
+        list($row, $column) = Coordinate::coordinateFromString($cell);
+        $row = Coordinate::columnIndexFromString($row);
+        $this->cells[$row][$column] = null;
+
+        return $this;
     }
 }
--- tests/DynamicBlockTest.php
+++ tests/DynamicBlockTest.php
@@ -28,4 +28,40 @@ final class DynamicBlockTest extends TestCase
         $block = new DynamicBlock();
         $this->assertEquals(0, $block->getWidth());
     }
+
+
+    /**
+     * Data provider for testing DynamicExelBlock::getHeight, see DynamicBlockTest::testGetHeight for expected data provider values
+     *
+     * @return array
+     */
+    public function getHeightDataProvider()
+    {
+        return [
+            'Test adding a cell with a null value still affects height' =>
+                ['addCell', ['E1', null], 5],
+            'Test adding a cell to the first row sets height to 1' =>
+                ['addCell', ['A1', 'test'], 1],
+            "Test adding a cell to any row greater than 1 sets the height to that cell's row" =>
+                ['addCell', ['AX1', 'test'], 50],
+        ];
+    }
+
+    /**
+     * Test DynamicBlock::getHeight for all functions that can modify a blocks height
+     *
+     * @dataProvider getHeightDataProvider
+     *
+     * @param  string $testing_function   Name of the function to call
+     * @param  array  $function_arguments Arguments to pass to the unction
+     * @param  int    $expected_height    The expected height of the block after the function call
+     *
+     * @return void
+     */
+    public function testGetHeight($testing_function, $function_arguments, $expected_height)
+    {
+        $block = new DynamicBlock();
+        $block->$testing_function(...$function_arguments);
+        $this->assertEquals($expected_height, $block->getHeight());
+    }
 }
