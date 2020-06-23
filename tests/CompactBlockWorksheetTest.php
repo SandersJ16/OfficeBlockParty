b2e8c64bffaf9230a38d97d2348a634b51fe29a9
Justin L Sanders <justin@pointw.com>
2020-06-23 00:10:16 -0500

    Modify Worksheet assertion to to only do a single assertion per test and give better information

--- tests/CompactBlockWorksheetTest.php
+++ tests/CompactBlockWorksheetTest.php
@@ -183,12 +183,40 @@ final class CompactBlockWorksheetTest extends TestCase
         $rendered_worksheet = $rendered_spreadsheet->getSheet(0);
         $rendered_cells = $rendered_worksheet->getCellCollection();
 
-        $error_message = "Failed XLSX file viewable at: '${file_location}'";
-        $this->assertEquals(count($expected_coordinate_values), count($rendered_cells->getCoordinates()), $error_message);
-        foreach ($expected_coordinate_values as $coordinate => $value) {
-            $this->assertTrue($rendered_cells->has($coordinate), $error_message);
-            $this->assertEquals($value, $rendered_cells->get($coordinate)->getValue());
+        $missing_cells = array();
+        $wrong_value_cells = array();
+        $extra_coordinates = array();
+        // Check that all corrdinates match our expected values
+        foreach ($expected_coordinate_values as $coordinate => $expected_value) {
+            if (!$rendered_cells->has($coordinate)) {
+                $missing_cells[$coordinate] = $expected_value;
+            } elseif (($actual_value = $rendered_cells->get($coordinate)->getValue()) != $expected_value) {
+                $wrong_value_cells[$coordinate] = ['expected' => $expected_value, 'actual' => $actual_value];
+            }
+        }
+        // Check if there are any unexpected coordinates
+        foreach (array_diff($rendered_cells->getCoordinates(), array_keys($expected_coordinate_values)) as $extra_coordinate) {
+            $extra_coordinates[$extra_coordinate] = $rendered_cells->get($extra_coordinate)->getValue();
+        }
+
+        if ($missing_cells || $wrong_value_cells || $extra_coordinates) {
+            $error_message = 'Rendered XLSX file did not match expected results:' . PHP_EOL;
+            foreach ($missing_cells as $coordinate => $value) {
+                $error_message .= '  -  ';
+                $error_message .= "Missing expected value '${value}' at coordinate ${coordinate}" . PHP_EOL;
+            }
+            foreach ($wrong_value_cells as $coordinate => $values) {
+                $error_message .= '  -  ';
+                $error_message .= "Expected value '${values['expected']}' for coordinate ${coordinate} but actual value was '${values['actual']}'" . PHP_EOL;
+            }
+            foreach ($extra_coordinates as $coordinate => $value) {
+                $error_message .= '  -  ';
+                $error_message .= "Expected coordinate ${coordinate} to not be set but had value of '${value}'" . PHP_EOL;
+            }
+            $error_message .= "Failed XLSX file viewable at: '${file_location}'";
+            $this->fail($error_message);
         }
         unlink($file_location);
+        $this->assertTrue(true);
     }
 }
